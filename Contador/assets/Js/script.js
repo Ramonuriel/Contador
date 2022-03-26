b4c578f64ed0bc09eddef8eab9a6582524233a60
Ramonuriel <Ramonuriel13@gmail.com>
2022-03-26 01:02:22 -0300

    1 Commit

---
+++ Contador/assets/Js/script.js
@@ -0,0 +1,14 @@
+console.log("Contador");
+
+var currentnumberWrapper = document.getElementById("currentnumber");
+var currentnumber = 0;
+
+function increment () {
+    currentnumber = currentnumber + 1;
+    currentnumberWrapper.innerHTML = currentnumber;
+}
+
+function decrement () {
+    currentnumber = currentnumber - 1;
+    currentnumberWrapper.innerHTML = currentnumber;
+}
